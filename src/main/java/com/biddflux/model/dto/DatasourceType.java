0947826a161d55f5637b10deb730393f2b745f97
biddflux.com <biddflux@gmail.com>
2024-08-04 03:44:25 +0300

    enum fixes

--- src/main/java/com/biddflux/model/dto/DatasourceType.java
+++ src/main/java/com/biddflux/model/dto/DatasourceType.java
@@ -1,5 +1,21 @@
 package com.biddflux.model.dto;
 
-public enum DatasourceType {
-	MYSQL;
+public enum DatasourceType implements KeyValuePair{
+	MYSQL("MySql");
+	private String val;
+
+	private DatasourceType(String val){
+		this.val = val;
+	}
+
+	@Override
+	public String getKey() {
+		return this.name();
+	}
+
+	@Override
+	public String getValue() {
+		return this.val;
+	}
+	
 }
