9bb1dc97343016d11204f1504cb4d9bc13ef8731
biddflux.com <biddflux@gmail.com>
2024-10-20 18:03:12 +0300

    Data implementation

--- src/main/java/com/biddflux/model/flow/factories/SourceFactory.java
+++ src/main/java/com/biddflux/model/flow/factories/SourceFactory.java
@@ -2,17 +2,19 @@ package com.biddflux.model.flow.factories;
 
 import java.util.Map;
 import java.util.function.Function;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.biddflux.model.flow.db.mysql.DataSourceFactoryMySql;
-import com.biddflux.model.flow.in.MySqlBackupProperties;
 import com.biddflux.model.flow.in.MySqlBackup;
+import com.biddflux.model.flow.in.MySqlBackupProperties;
 import com.biddflux.model.flow.in.Source;
 import com.biddflux.model.flow.in.StoredData;
 import com.biddflux.model.flow.out.Storage;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import lombok.Getter;
 
@@ -20,6 +22,7 @@ public class SourceFactory extends AbstractFactory<Source>{
 	@Autowired
 	private ObjectMapper objectMapper;
 	
+	@SuppressWarnings("unchecked")
 	@Getter
 	private Map<String, Function<JsonNode, Source>> builders = Map.of(
 			"MySqlBackup", node -> {
@@ -35,8 +38,20 @@ public class SourceFactory extends AbstractFactory<Source>{
 				String version = node.get("version").asText();
 				sd.setVersion(version);
 				JsonNode tagsNode = node.get("tags");
-				@SuppressWarnings("unchecked")
-				Map<String, String> tags = objectMapper.convertValue(tagsNode, Map.class);
+				Map<String, String> tags = new HashMap<>();
+				if(tagsNode.isArray()){
+					ArrayNode tagsArr = (ArrayNode) tagsNode;
+					for(int i=0; i < tagsArr.size(); i++){
+						JsonNode tNode = tagsArr.get(i);
+						String tName = tNode.findValue("name").asText(null);
+						String tValue = tNode.findValue("value").asText(null);
+						if(tName != null && tValue != null){
+							tags.put(tName, tValue);
+						}
+					}
+				}else{
+					tags = objectMapper.convertValue(tagsNode, Map.class);
+				}
 				sd.setTags(tags);
 				return sd;
 			}
