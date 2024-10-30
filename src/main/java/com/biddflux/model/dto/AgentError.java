69d9785b0ba4e62f6137da38da16580d7c51a02f
biddflux.com <biddflux@gmail.com>
2024-10-31 02:02:27 +0300

    agent error model

--- src/main/java/com/biddflux/model/dto/AgentError.java
+++ src/main/java/com/biddflux/model/dto/AgentError.java
@@ -1,6 +1,10 @@
 package com.biddflux.model.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import com.biddflux.commons.persistence.BaseDto;
+import com.biddflux.model.dto.agent.AgentReference;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,9 +13,13 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class AgentError extends BaseDto<Long>{
     private Long agentId;
-	private boolean read;
+	private boolean seen;
 	private String type;
 	private String name;
 	private String message;
     private String error;
+	private LocalDate date;
+	private LocalTime time;
+	private String ago;
+	private AgentReference agent;
 }
---
+++ src/main/java/com/biddflux/model/dto/AgentErrorDetail.java
@@ -0,0 +1,14 @@
+package com.biddflux.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AgentErrorDetail {
+    private String errorDetail;
+}
