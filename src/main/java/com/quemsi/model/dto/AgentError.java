7221171a5aac0abda6d5b8fd0b4f0eed9535ef46
quemsi <quemsi.app@gmail.com>
2025-09-27 13:18:52 +0300

    native api fixes

--- src/main/java/com/quemsi/model/dto/AgentError.java
+++ src/main/java/com/quemsi/model/dto/AgentError.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import com.quemsi.commons.persistence.BaseDto;
-import com.quemsi.model.dto.agent.AgentReference;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Builder
 public class AgentError extends BaseDto<Long>{
     private Long agentId;
 	private boolean seen;
@@ -21,5 +22,5 @@ public class AgentError extends BaseDto<Long>{
 	private LocalDate date;
 	private LocalTime time;
 	private String ago;
-	private AgentReference agent;
+	private ObjectReference agent;
 }
