e3443f430ab5aad42125d3362fe69a62ce0ee32f
quemsi <quemsi.app@gmail.com>
2026-03-01 01:33:12 +0300

    serializable

--- src/main/java/com/quemsi/model/dto/AgentStatusUpdate.java
+++ src/main/java/com/quemsi/model/dto/AgentStatusUpdate.java
@@ -1,5 +1,7 @@
 package com.quemsi.model.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AgentStatusUpdate {
+public class AgentStatusUpdate implements Serializable {
     private Long agentId;
     private AgentStatus status;
 }
